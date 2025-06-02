a9074cefc3cb75e52223ec8712708da8a16e8302
kdpavlov <112274429+kdpavlov@users.noreply.github.com>
2025-06-02 22:03:43 +0300

    static

--- KR20252/settings.py
+++ KR20252/settings.py
@@ -16,8 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/Media/'
-MEDIA_ROOT = os.path.join (BASE_DIR, 'Media')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
